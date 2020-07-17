6cb155d6ac0b79ba568ab98f64a2d0900596aad5
M98B <39217682+M98B@users.noreply.github.com>
2020-07-17 02:03:41 +0200

    Add files via upload

--- chrono.py
+++ chrono.py
@@ -5,6 +5,9 @@ cwd = input('Folder name:')
 if cwd:
     os.chdir(cwd)
 
+name_convention = input("Give the naming convention")
+
+starting_index = 1
 image_class = '_2yuc _3-96'
 html_data = open('message_1.html', 'r', errors='ignore', encoding='utf-8')
 soup = BeautifulSoup(html_data, 'html.parser')
@@ -12,23 +15,24 @@ images = soup.find_all(attrs={'class': image_class})
 
 paths = []
 
-for image in images:
+for image in reversed(images):
     if 'photos' in image['src']:
         paths.append(image['src'])
 
 
-for i, path in enumerate(reversed(paths), start=1):
+for i, path in enumerate(paths, start=starting_index):
     old_name = 'photos/' + os.path.basename(path)
     new_name = ''
     if '.png' in path:
-        new_name = f'photos/{i}.png'
+        new_name = f'photos/{name_convention}_{i}.png'
     elif '.jpg' in path:
-        new_name = f'photos/{i}.jpg'
+        new_name = f'photos/{name_convention}_{i}.jpg'
     else:
         print('No file extension')
         break
-    #print(f'Renaming {old_name} to {new_name}')
+
     os.rename(old_name, new_name)
 
 html_data.close()
+
 
